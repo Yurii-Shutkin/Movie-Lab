0b2899adc04280bcb313765301e852a1caf5fc72
Yurii-Shutkin <yurashutkinn@gmail.com>
2023-03-28 23:23:38 +0300

    add a var with apiKey

--- movie-lab/src/server.js
+++ movie-lab/src/server.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+const apiKey = process.env.REACT_APP_API_KEY;
 const API_URL = 'https://api.themoviedb.org/3';
 const MOVIE_PATH = '/discover/movie?';
 const SEARCH_PATH = '/search/movie?';
@@ -8,7 +9,7 @@ export const server = {
     getMovies: async (state) => {
         const { data: {results} } = await axios.get(`${API_URL}${MOVIE_PATH}`, {
             params: {
-                api_key: process.env.REACT_APP_API_KEY
+                api_key: apiKey
             }
         })
         state(results)
@@ -17,7 +18,7 @@ export const server = {
     findMovie: async (title, state) => {
         const data = await axios.get(`${API_URL}${SEARCH_PATH}`, {
             params: {
-                api_key: process.env.REACT_APP_API_KEY,
+                api_key: apiKey,
                 query: title
             }
         })
