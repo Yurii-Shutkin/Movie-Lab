f82222de6cdd3ad638d1870dd8bf1dbd7cfee10b
Yurii-Shutkin <yurashutkinn@gmail.com>
2023-03-28 23:22:22 +0300

    add an image-plug

--- movie-lab/src/components/MovieCard/MovieCard.js
+++ movie-lab/src/components/MovieCard/MovieCard.js
@@ -6,7 +6,12 @@ export default function MovieCard({title, img}) {
 
     return (
         <div className='movie-card'>
-            {img ? <img  className='movie-poster' src={ IMG_PATH + img } alt={ title }/> : null}
+            {img ? 
+                <img  className='movie-poster' src={ IMG_PATH + img } alt={ title }/> 
+                :
+                <div className='movie-poster-plug'>
+                    <span>No image</span>
+                </div>}
             <h4 className='movie-title'>
                 {title}
             </h4>
